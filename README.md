dc51f039c85635e05dec45839dc944a69707f84c
Piri <pkamalarubirasan@onyxcapitaltechnology.com>
2024-12-07 17:55:35 +0000

    Use Volumes to change input files and refresh data

--- README.md
+++ README.md
@@ -9,9 +9,11 @@ Analyse portfolio of long only equities.
 ### Using Docker
 1. Clone the repository
 2. Go to the cloned repository directory
-3. Build the Docker image and run the container:
+3. Create a folder named inputs and drop your portfolio csvs here
+4. Build the Docker image and run the container:
 ```bash
 docker build -t portfolioanalyser .
-docker run -p 8050:8050 portfolioanalyser
+docker run -p 8050:8050 -v $(pwd)/inputs:/app/docker_inputs portfolioanalyser
 ```
 5. In your browser go to: http://localhost:8050
+6. You can switch out the file in the inputs folder and refresh the data while the dashboard is running
--- dashboard.py
+++ dashboard.py
@@ -1,21 +1,50 @@
 import dash
-from dash import html, dash_table
+from dash import html, dash_table, Input, Output
 import pandas as pd
-
-data = pd.read_csv('./inputs/portfolio.csv')
+import os
 
 app = dash.Dash(__name__)
 server = app.server
 
+inputs_folder = "./docker_inputs"
+
+def load_data():
+    filenames = [f for f in os.listdir(inputs_folder) if f.endswith('.csv')]
+    if filenames:
+        filename = filenames[0]
+        df = pd.read_csv(f'{inputs_folder}/{filename}')
+        cols = [{"name": i, "id": i} for i in df.columns]
+        data = df.to_dict('records')
+    else:
+        cols = []
+        data = []
+
+    return cols, data
+
+cols, data = load_data()
+
 app.layout = html.Div([
     html.H1("Portfolio Analysis"),
+    html.Button("Refresh Data", id="refresh-button", n_clicks=0),
     dash_table.DataTable(
         id='data-table',
-        columns=[{"name": i, "id": i} for i in data.columns],
-        data=data.to_dict('records'),
+        columns=cols,
+        data=data,
         style_table={'width': '75%'}
     )
 ])
+
+# Callback to refresh the data
+@app.callback(
+    [Output("data-table", "columns"),
+     Output("data-table", "data"),
+     ],
+    Input("refresh-button", "n_clicks")
+)
+def refresh_data(n_clicks):
+    cols, data = load_data()
+    
+    return cols, data
 
 if __name__ == '__main__':
     app.run_server(debug=True, host='0.0.0.0', port=8050)
